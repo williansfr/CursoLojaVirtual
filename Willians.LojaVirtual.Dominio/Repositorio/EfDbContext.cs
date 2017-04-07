5f1c53648165ec35989b628ed542701c0a932f50
Willians Ferreira <williansfr@hotmail.com>
2017-04-07 16:11:33 -0300

    Entity

---
+++ Willians.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Willians.LojaVirtual.Dominio.Entidade;
+
+namespace Willians.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
---
+++ Willians.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Willians.LojaVirtual.Dominio.Repositorio;
+
+namespace Willians.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ProdutosRepositorio _produtoRepositorio = new ProdutosRepositorio();
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            var produtos = _produtoRepositorio.Produtos.Take(10);
+            return View(produtos);
+        }
+    }
+}
