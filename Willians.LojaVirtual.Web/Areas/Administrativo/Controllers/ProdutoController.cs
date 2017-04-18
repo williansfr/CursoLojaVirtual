36674a45a3897a8068a5372d48879788226542ad
Willians Ferreira <williansfr@hotmail.com>
2017-04-18 14:47:06 -0300

    Area Administrativa

---
+++ Willians.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Willians.LojaVirtual.Dominio.Repositorio;
+
+namespace Willians.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        // GET: Administrativo/Produto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
