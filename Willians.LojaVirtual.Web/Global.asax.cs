4b0b58acce98da7f886a0a99e77ca9ad91c39c74
Willians Ferreira <williansfr@hotmail.com>
2017-04-24 23:28:27 -0300

    Custom Model Binder

--- Willians.LojaVirtual.Web/Global.asax.cs
+++ Willians.LojaVirtual.Web/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Willians.LojaVirtual.Dominio.Entidade;
+using Willians.LojaVirtual.Web.Infraestrutura;
 
 namespace Willians.LojaVirtual.Web
 {
@@ -13,6 +15,7 @@ namespace Willians.LojaVirtual.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(Carrinho), new CarrinhoModelBinder());
         }
     }
 }
