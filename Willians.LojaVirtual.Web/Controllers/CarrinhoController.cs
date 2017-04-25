73ff4c1c237da645314d2f05d0284c332e68c199
Willians Ferreira <williansfr@hotmail.com>
2017-04-24 23:48:24 -0300

    Controller Carrinho

--- Willians.LojaVirtual.Web/Controllers/CarrinhoController.cs
+++ Willians.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -14,56 +14,13 @@ namespace Willians.LojaVirtual.Web.Controllers
     {
         private ProdutosRepositorio _produtoRepositorio = new ProdutosRepositorio();
 
-        // GET: Carrinho
-        public RedirectToRouteResult Adicionar(int produtoId, string returnUrl)
+        public ViewResult Index(Carrinho carrinho, string returnUrl)
         {
-            Produto produto = _produtoRepositorio.Produtos.FirstOrDefault(d => d.ProdutoId == produtoId);
+            var carrinhoViewModel = new CarrinhoViewModel();
+            carrinhoViewModel.Carrinho = carrinho; // ObterCarrinho();
+            carrinhoViewModel.ReturnUrl = returnUrl;
 
-            if (produto != null)
-            {
-                ObterCarrinho().AdicionarItem(produto, 1);
-            }
-
-            return RedirectToAction("Index", new { returnUrl });
-        }
-
-        private Carrinho ObterCarrinho()
-        {           
-            if (Session["Carrinho"] == null) {
-                Session["Carrinho"] = new Carrinho();
-            }
-
-            Carrinho carrinho = (Carrinho)Session["Carrinho"];
-            
-            return carrinho;
-        }
-
-        // GET: Carrinho
-        public RedirectToRouteResult Remover(int produtoId, string returnUrl)
-        {
-            Produto produto = _produtoRepositorio.Produtos.FirstOrDefault(d => d.ProdutoId == produtoId);
-
-            if (produto != null)
-            {
-                ObterCarrinho().RemoverItem(produto);
-            }
-
-            return RedirectToAction("Index", new { returnUrl });
-        }
-
-        public ViewResult Index(string returnUrl)
-        {
-            var carrinho = new CarrinhoViewModel();
-            carrinho.Carrinho = ObterCarrinho();
-            carrinho.ReturnUrl = returnUrl;
-
-            return View(carrinho);
-        }
-
-        public PartialViewResult Resumo()
-        {
-            Carrinho carrinho = ObterCarrinho();
-            return PartialView(carrinho);
+            return View(carrinhoViewModel);
         }
 
         public ViewResult FecharPedido()
@@ -72,9 +29,9 @@ namespace Willians.LojaVirtual.Web.Controllers
         }
 
         [HttpPost]
-        public ViewResult FecharPedido(Pedido pedido)
+        public ViewResult FecharPedido(Carrinho carrinho, Pedido pedido)
         {
-            Carrinho carrinho = ObterCarrinho();
+            //Carrinho carrinho = ObterCarrinho();
             EmailConfiguracoes emailConfig = new EmailConfiguracoes();
             emailConfig.EscreverArquivo = bool.Parse(ConfigurationManager.AppSettings["Email.EscreverArquivo"]);
 
@@ -83,13 +40,56 @@ namespace Willians.LojaVirtual.Web.Controllers
             if (!carrinho.ItensDoCarrinho().Any())
                 ModelState.AddModelError("", "Carrinho vazio! \nPedido não pode ser concluído!");
 
-            if (ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
                 emailPedido.ProcessarPedido(carrinho, pedido);
                 carrinho.LimparCarrinho();
                 return View("PedidoConcluido");
             }
             else
                 return View(pedido);
+        }
+
+        public PartialViewResult Resumo(Carrinho carrinho)
+        {
+            //Carrinho carrinho = ObterCarrinho();
+            return PartialView(carrinho);
+        }
+        // GET: Carrinho
+        public RedirectToRouteResult Adicionar(Carrinho carrinho, int produtoId, string returnUrl)
+        {
+            Produto produto = _produtoRepositorio.Produtos.FirstOrDefault(d => d.ProdutoId == produtoId);
+
+            if (produto != null)
+            {
+                carrinho.AdicionarItem(produto, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        //private Carrinho ObterCarrinho()
+        //{           
+        //    if (Session["Carrinho"] == null) {
+        //        Session["Carrinho"] = new Carrinho();
+        //    }
+
+        //    Carrinho carrinho = (Carrinho)Session["Carrinho"];
+            
+        //    return carrinho;
+        //}
+
+        // GET: Carrinho
+        public RedirectToRouteResult Remover(Carrinho carrinho, int produtoId, string returnUrl)
+        {
+            Produto produto = _produtoRepositorio.Produtos.FirstOrDefault(d => d.ProdutoId == produtoId);
+
+            if (produto != null)
+            {
+                carrinho.RemoverItem(produto);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
         }
 
         public ViewResult PedidoConcluido()
