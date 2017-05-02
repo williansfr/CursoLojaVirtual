a96b106960935eeb60de39861b6a177f69c256fb
Willians Ferreira <williansfr@hotmail.com>
2017-05-02 09:28:38 -0300

    Action Categoria

--- Willians.LojaVirtual.Web.V2/Controllers/NavController.cs
+++ Willians.LojaVirtual.Web.V2/Controllers/NavController.cs
@@ -26,11 +26,45 @@ namespace Willians.LojaVirtual.Web.V2.Controllers
         }
 
         [Route("nav/{id}/{marca}")]
-        public ActionResult ObterProdutosPorMarcas(string id)
+        public ActionResult ObterProdutosPorMarcas(string id, string marca)
         {
-            var model = new ProdutosViewModel {Produtos = null};
+            _repositorio = new ProdutoModeloRepositorio();
 
-            return View(model);
+            var produtos = _repositorio.ObterProdutosVitrine(marca: id);
+
+            _model = new ProdutosViewModel();
+            _model.Produtos = produtos;
+            _model.Titulo = marca;
+
+            return View("Navegacao", _model);
+        }
+
+        [Route("nav/times/{id}/{clube}")]
+        public ActionResult ObterProdutosPorClubes(string id, string clube)
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+
+            var produtos = _repositorio.ObterProdutosVitrine(linha: id);
+
+            _model = new ProdutosViewModel();
+            _model.Produtos = produtos;
+            _model.Titulo = clube;
+
+            return View("Navegacao", _model);
+        }
+
+        [Route("nav/genero/{id}/{genero}")]
+        public ActionResult ObterProdutosPorGenero(string id, string genero)
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+
+            var produtos = _repositorio.ObterProdutosVitrine(genero: id);
+
+            _model = new ProdutosViewModel();
+            _model.Produtos = produtos;
+            _model.Titulo = genero;
+
+            return View("Navegacao", _model);
         }
     }
 }
